39111045b7886d151c442bd5952e53b3c4395586
Benoit Lepage <workaholiks@gmail.com>
2025-09-01 15:54:46 +0200

    init

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
 <?php
 
-Timber::render( 'index.twig' );
+use Timber\Timber;
+$context = Timber::context();
+
+// Récupérer les posts pour la page d'accueil
+$context['posts'] = Timber::get_posts([
+    'post_type' => 'post',
+    'posts_per_page' => -1,
+    'post_status' => 'publish'
+]);
+
+// Une Query de 5 articles
+// dans une liste comprenant chaque 
+
+Timber::render( 'index.twig', $context );
